b4cca0a3866074668a760ae44358cf2073a1fe6c
aldrsun <ali.dursun@metu.edu.tr>
2025-02-01 16:26:21 +0300

    Trying a different packet

--- src/buttonCall/buttonCall.controller.ts
+++ src/buttonCall/buttonCall.controller.ts
@@ -1,12 +1,12 @@
 import { ButtonCallService } from './buttonCall.service';
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
 
 @Controller('button-calls')
 export class ButtonCallController {
   constructor(private readonly buttonCallService: ButtonCallService) {}
 
   @Post()
-  createButtonCall(@Param('code') code: number) {
-    return this.buttonCallService.handleButtonCall(code);
+  createButtonCall(@Body() body: { code: number }) {
+    return this.buttonCallService.handleButtonCall(body.code);
   }
 }
