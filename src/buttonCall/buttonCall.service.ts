ae44a0ab912f5974cf396af51170e1ee1c8b0439
aldrsun <ali.dursun@metu.edu.tr>
2025-02-05 15:51:58 +0300

    Adding requests

--- src/buttonCall/buttonCall.service.ts
+++ src/buttonCall/buttonCall.service.ts
@@ -1,9 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { format } from 'date-fns';
 import { tableCodes } from './tableCodes.mapping';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class ButtonCallService {
+  constructor(private readonly httpService: HttpService) {}
   handleButtonCall(code: number) {
     const tableEntry = Object.entries(tableCodes).find(
       ([, value]) => value.call_code === code || value.cancel_code === code,
@@ -16,8 +18,38 @@ export class ButtonCallService {
     const [tableName, { call_code, cancel_code }] = tableEntry;
     if (code == call_code) {
       console.log('New Call!', tableName, call_code);
+      try {
+        const response = lastValueFrom(
+          this.httpService.post(
+            'http://localhost:3001/button-calls',
+            { location: '2', tableName: tableName },
+            {
+              headers: { 'Content-Type': 'application/json' },
+            },
+          ),
+        );
+
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
     } else if (code == cancel_code) {
       console.log('Cancel Call!', tableName, cancel_code);
+      try {
+        const response = lastValueFrom(
+          this.httpService.patch(
+            'http://localhost:3001/button-calls',
+            { location: '2', tableName: tableName },
+            {
+              headers: { 'Content-Type': 'application/json' },
+            },
+          ),
+        );
+
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
     }
 
     return HttpStatus.OK;
