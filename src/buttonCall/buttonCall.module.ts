1f566d6329937a967e1939f34e71da064f5a4283
aldrsun <ali.dursun@metu.edu.tr>
2025-02-05 16:28:06 +0300

    Adding requests

--- src/buttonCall/buttonCall.module.ts
+++ src/buttonCall/buttonCall.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ButtonCallController } from './buttonCall.controller';
 import { ButtonCallService } from './buttonCall.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [ButtonCallService],
-  exports: [ButtonCallService],
+  exports: [],
   controllers: [ButtonCallController],
 })
 export class ButtonCallModule {}
