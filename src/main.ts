53c21ce9fa07bb01771c7e14a496319970019da7
aldrsun <ali.dursun@metu.edu.tr>
2025-02-19 17:41:14 +0300

    Temporary serve logs for development purposes

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 require('dotenv').config();
 
+import * as express from 'express';
+import * as path from 'path';
+import * as os from 'os';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/logs', express.static(path.join(os.homedir(), 'log')));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
